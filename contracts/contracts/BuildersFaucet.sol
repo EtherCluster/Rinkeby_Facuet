59ce884d76275beca2ec4b486b8d320b5e5a0dbf
EtherCluster <ethercluster@protonmail.com>
2021-11-03 12:19:18 +0200

    Updated the contract-made a lot of changes

--- contracts/contracts/BuildersFaucet.sol
+++ contracts/contracts/BuildersFaucet.sol
@@ -5,40 +5,87 @@ pragma solidity ^0.8.0;
 // import "hardhat/console.sol";
 
 contract BuildersFaucet {
+    //total funds in faucet at the moment
     uint256 public totalFunds;
+    //total funds sent to an address per given request
     uint256 public payOutAmt;
-    uint public totalInteractions;
-    address[] public contributors;
+    //total amount of contributos
+    uint256 public totalContributors;
+    
+    //total amount Contributed
+        uint public totalContributed;
+        
+       //total amount paid to users
+        uint public totalRequested;
+            //total times paid to users
+        uint public totalRequests;
+    //
+    struct Contributor{
+        uint amtRequested;
+        uint amtDeposited;
+        uint lastTimeSentAt;
+        bool alreadyContributed;
+    
+    }
+    
+    
+    //mapping user address to user(contributor) struct
+     mapping(address => Contributor) public contributors;
+     
+     
+    // timestamp when a certain addresss requested funds
     mapping(address => uint) public lastTimeSentAt;
 
-    event Deposited(address indexed userAddress, uint256 weiAmount, uint totalInteractions, uint256 totalFunds, uint totalInteractors);
-    event TokensSent(address indexed userAddress, uint256 weiAmount, uint totalInteractions, uint256 totalFunds);
 
+//deopist event
+    event Deposited(address indexed userAddress, uint256 weiAmount, uint256 totalFunds, uint totalInteractors);
+//requested event
+    event TokensSent(address indexed userAddress, uint256 weiAmount,  uint256 totalFunds);
+//contract owner
     address public owner;
 
     constructor() payable {
         totalFunds = 0;
-        payOutAmt = 50000000000000000;
+        totalContributors = 0;
+        totalRequested =0;
+        totalRequests = 0;
+        totalContributed =0;
+        payOutAmt = 50000000000000000; //default on launch ( will be chanegeable via function)
         owner = msg.sender;
     }
 
     //lets user deposit to the contract
     function deposit() public payable {
-        totalInteractions = totalInteractions + 1;
+    
         totalFunds = totalFunds + msg.value;
-
-        bool contributorExist = false;
-        for (uint i=0; i<contributors.length; i++) {
-            if (contributors[i] == msg.sender) {
-                contributorExist = true;
-            }
+  
+        if(contributors[msg.sender].alreadyContributed == false){
+            
+    //update public variables        
+            totalContributors = totalContributors+1;
+            totalContributed = totalContributed+msg.value;
+            
+    //update user variables        
+            contributors[msg.sender].alreadyContributed = true;
+            contributors[msg.sender].amtDeposited = contributors[msg.sender].amtDeposited + msg.value;
+            
+        }else{
+            
+            //update public variables        
+            totalContributed = totalContributed+msg.value;
+            
+    //update user variables        
+            contributors[msg.sender].amtDeposited = contributors[msg.sender].amtDeposited + msg.value;
+            
         }
-        if (!contributorExist) {
-            contributors.push(msg.sender);
-        }
-
-        emit Deposited(msg.sender, msg.value, totalInteractions, totalFunds, contributors.length);
+        
+        
+        emit Deposited(msg.sender, msg.value, totalFunds,totalContributors);
     }
+    
+    
+    
+    
 
     //functio where owners can set payout amount
     function setPayoutAmt(uint256 weiAmtPayout) public{
@@ -46,7 +93,7 @@ contract BuildersFaucet {
         payOutAmt = weiAmtPayout;
     }
 
-
+//for backend purposes only
     function getTimeToWaitUntilNextRequest(address userAddress) public view returns (uint256) {
         uint256 canSendMoney = 0;
         if (lastTimeSentAt[userAddress] > 0) {
@@ -58,17 +105,25 @@ contract BuildersFaucet {
     }
 
     //this will pay out users who request -- the reason we have address as input paramter and not msg.sender is becasue we will use web3 on the frontend to get the user's address
-    function sendTokensToAddress(address userAddress) public {
-        uint256 timeToWait = getTimeToWaitUntilNextRequest(userAddress);
-        if (timeToWait > 0) {
-            emit TokensSent(userAddress, 0, totalInteractions, totalFunds);
-        } else {
-            require(payable(userAddress).send(payOutAmt));
-            lastTimeSentAt[userAddress] = block.timestamp;
-            totalInteractions = totalInteractions + 1;
-            totalFunds = totalFunds - payOutAmt;
-            emit TokensSent(userAddress, payOutAmt, totalInteractions, totalFunds);
-        }
+    function sendTokensToAddress(address userAddress) public payable {
+        require((block.timestamp - contributors[msg.sender].lastTimeSentAt) > 1 days);
+        
+           require(payable(userAddress).send(payOutAmt));
+           
+           
+             //update public variables        
+       totalRequested = totalRequested+payOutAmt;
+       totalRequests = totalRequests++;
+            
+    //update user variables        
+         
+            contributors[msg.sender].amtRequested = contributors[msg.sender].amtRequested + msg.value;
+            contributors[msg.sender].lastTimeSentAt = block.timestamp;
+           
+        
+            emit TokensSent(userAddress, payOutAmt, totalFunds);
+        
+        
     }
 
     //returns total fund sin contract
@@ -80,12 +135,12 @@ contract BuildersFaucet {
     //returns total contributors
     function getTotalContributors() public view returns (uint256) {
         // console.log("We have %d total funds!", totalFunds);
-        return contributors.length;
+        return totalContributors;
     }
 
     //returns total requests
     function getTotalRequests() public view returns (uint256) {
         // console.log("We have %d total funds!", totalFunds);
-        return totalInteractions;
+        return totalRequested;
     }
 }
